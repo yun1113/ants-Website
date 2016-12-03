52325a33d8fd89bab372b0d8bb545556d14e8963
yun1113 <yunyun1113c@gmail.com>
2016-12-03 17:03:44 +0800

    Feature: Reorder html and add contact

--- project/vmp/vmp/settings.py
+++ project/vmp/vmp/settings.py
@@ -146,3 +146,9 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+DEFAULT_FROM_EMAIL = 'testing@example.com'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
+EMAIL_USE_TLS = False
+EMAIL_PORT = 1025
