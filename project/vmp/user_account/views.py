9c25c12e95bba4a75b43259f3d55094ed4dc1171
ants <ants@ants.(none)>
2017-02-11 15:22:51 +0800

    login after signup

--- project/vmp/user_account/views.py
+++ project/vmp/user_account/views.py
@@ -12,20 +12,18 @@ from datetime import datetime
 
 
 def register(request):
+
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
             user_account = UserAccount.objects.create(user=user, create_date=datetime.now())
 
-        form = MalwareUploadFileForm()
-        malware_num = len(Malware.objects.filter(~Q(detectionrate=0)))
-        upload_num = len(Upload.objects.all())
-        analysis_num = len(Malware.objects.all())
-        signup_form = UserCreationForm()
-        login_form = AuthenticationForm()
+            user = auth.authenticate(username=form.cleaned_data['username'], password=form.cleaned_data['password1'])
+            if user is not None and user.is_active:
+                auth.login(request, user)
 
-    return render(request, 'malware.html', locals())
+    return HttpResponseRedirect('/malwareupload/')
 
 
 def login(request):
