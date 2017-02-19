844b17c462f2ae2681dc54a1e842d1e4101a0955
yun1113 <yunyun1113c@gmail.com>
2017-02-19 10:33:40 +0800

    multiple form handling

--- project/vmp/vmp/urls.py
+++ project/vmp/vmp/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url
 from django.contrib import admin
-from malwaredb.views import malware_search, malware_upload, malware_detail, \
+from malwaredb.views import malware_search, get_index, malware_detail, \
     malware_download, load_hooklog_page, contact, malware_family, malware_behavior
 from user_account.views import register, logout, login
 from django.conf.urls import handler400, handler403, handler404, handler500
@@ -11,7 +11,7 @@ handler500 = 'malwaredb.views.server_error'
 urlpatterns = [
 
     url(r'^admin/', admin.site.urls),
-    url(r'^malwareupload/$', malware_upload),  # upload
+    url(r'^malwareupload/$', get_index),  # upload
     url(r'^malwaredb/$', malware_search),  # search
     url(r'^malwaredetail/$', malware_detail),  # detail
     url(r'^malwaredownload/$', malware_download),  # download
@@ -19,8 +19,8 @@ urlpatterns = [
     url(r'^malwarebehavior/$', malware_behavior),
     # url(r'^malwarefamily2/$', malware_family2),
     url(r'^contact/$', contact),
-    url(r'^register/$', register),
-    url(r'^login/$', login),
+    url(r'^register/$', get_index),
+    url(r'^login/$', get_index),
     url(r'^logout/$', logout),
 
     url(r'^analysis/(?P<hash_value>\w+)/(?P<page>\w+)$', load_hooklog_page),  # detail hooklog tab
