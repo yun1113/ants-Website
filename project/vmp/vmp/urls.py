4b8a545374d6b7be7e52f9a8b7dfb04fcd685ac0
ants <yunyun1113c@gmail.com>
2017-02-28 15:02:06 +0800

    fix

--- project/vmp/vmp/urls.py
+++ project/vmp/vmp/urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls import url, handler400, handler403, handler404, handler500
 from django.contrib import admin
 
-from malwaredb.views import malware_search, malware_upload, malware_detail, \
-    malware_download, load_hooklog_page, contact, malware_family, malware_behavior, malware_signup
-from user_account.views import register, logout, login
+from malwaredb.views import malware_search, get_index, malware_detail, \
+    malware_download, load_hooklog_page, contact, malware_family, malware_behavior
+from user_account.views import logout
 
 
 handler404 = 'malwaredb.views.bad_request'
@@ -12,17 +12,16 @@ handler500 = 'malwaredb.views.server_error'
 urlpatterns = [
 
     url(r'^admin/', admin.site.urls),
-    url(r'^index/$', malware_upload),  # upload
+    url(r'^index/$', get_index),  # login, signup, upload
+
     url(r'^malwaredb/$', malware_search),  # search
     url(r'^malwaredetail/$', malware_detail),  # detail
     url(r'^malwaredownload/$', malware_download),  # download
+
     url(r'^malwarefamily/$', malware_family),
     url(r'^malwarebehavior/$', malware_behavior),
-    url(r'^signup/$', malware_signup),
-    # url(r'^malwarefamily2/$', malware_family2),
+
     url(r'^contact/$', contact),
-    url(r'^register/$', malware_upload),
-    url(r'^login/$', malware_upload),
     url(r'^logout/$', logout),
 
     url(r'^analysis/(?P<hash_value>\w+)/(?P<page>\w+)$', load_hooklog_page),  # detail hooklog tab
