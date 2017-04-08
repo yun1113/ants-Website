12c4239d0f31d161d3c89a8ebf3c6ea9ed05e0c3
yun1113 <yunyun1113c@gmail.com>
2017-04-08 09:17:06 -0400

    finish streaming

--- project/vmp/automation/script/profiling.sh
+++ project/vmp/automation/script/profiling.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-python /home/root/automation/script/winmalware.py /home/root/malware/$1 /home/root/automation/output/
-python /home/root/automation/script/moving.py $1
+if $2
+then
+    echo $1 $3 $4
+    python /home/root/automation/script/watch.py $1 $3 $4 &
+fi
+(python /home/root/automation/script/winmalware.py /home/root/malware/$1 /home/root/automation/output/; python /home/root/automation/script/moving.py $1)
