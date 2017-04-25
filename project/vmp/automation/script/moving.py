50b6298f5b4a330416296d1bcab2e31e57342991
yun1113 <yunyun1113c@gmail.com>
2017-04-25 11:06:54 -0400

    remove .trace file

--- project/vmp/automation/script/moving.py
+++ project/vmp/automation/script/moving.py
@@ -4,7 +4,6 @@ import os
 
 
 for filename in os.listdir("/home/root"):
-    if "malware.exe_" in filename:
+    if "malware.exe_" in filename and not filename.endswith(".trace"):
         print("mv " + filename)
-        print(sys.argv)
         call(["mv", os.path.join("/home/root/" + filename), os.path.join("/home/root/automation/output/" + sys.argv[1] + "_" + filename.split("_")[1])])
