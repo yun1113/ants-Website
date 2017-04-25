2fa7ba0a7c8b4b741100232936d0bc49dd322dad
yun1113 <yunyun1113c@gmail.com>
2017-04-25 10:06:13 -0400

    fix moving.py

--- project/vmp/automation/script/moving.py
+++ project/vmp/automation/script/moving.py
@@ -6,6 +6,5 @@ import os
 for filename in os.listdir("/home/root"):
     if "malware.exe_" in filename:
         print("mv " + filename)
-        if "hooklog" in filename:
-            call(["mv", os.path.join("/home/root/" + filename),
-                  os.path.join("/home/root/automation/output/" + sys.argv[2] + "_" + filename.split("_")[1])])
+        print(sys.argv)
+        call(["mv", os.path.join("/home/root/" + filename), os.path.join("/home/root/automation/output/" + sys.argv[1] + "_" + filename.split("_")[1])])
